45f28981795bab06d5ab6107807a8708a8b975a8
emilia-klekotka <cafemcia@gmail.com>
2021-11-26 12:49:04 +0100

    Initial commit

--- src/main/java/pl/maciek/pizza_app/domain/model/OrderStatusType.java
+++
@@ -1,7 +0,0 @@
-package pl.maciek.pizza_app.domain.model;
-
-public enum OrderStatusType {
-
-    NEW, IN_PROGRESS, READY, OUT_FOR_DELIVERY, DELIVERED, CANCELED
-
-}
--- src/main/java/pl/maciek/pizza_app/domain/model/SizeType.java
+++
@@ -1,7 +0,0 @@
-package pl.maciek.pizza_app.domain.model;
-
-public enum SizeType {
-
-    S, M, L
-
-}
